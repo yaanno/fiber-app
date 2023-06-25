60399dddafe3e182f01bcf133e7928d6b62f46f7
Janos Hardi <yaanno@gmail.com>
2023-06-25 21:39:14 +0200

    init

---
+++ server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"title": "hello",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
